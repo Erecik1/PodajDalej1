b485eb7e9c24988d9c821a67e268b34c3b6e1eff
Erecik <eret112@gmail.com>
2021-05-17 12:33:39 +0200

    work

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'grm@_$#@#i$*wm)ct235ln4jg1jxd$^tf&y2wyt5@s%(im)ckr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.246.20.191']
 
 
 # Application definition
